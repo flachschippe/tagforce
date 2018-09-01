6c32f62f76867aa0a0d1c1c5ff05e01b97d9d13e
flachschippe <flachschippe@web.de>
2018-09-01 22:27:48 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ForcegraphComponent } from './forcegraph/forcegraph.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForcegraphComponent
   ],
   imports: [
     BrowserModule,
